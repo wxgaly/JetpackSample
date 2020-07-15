264ada8d52accfe39e1b282063dfe6b9a3549e1e
wangxuguang <nova_wangxuguang@126.com>
2020-07-15 23:58:44 +0800

    增加data binding sample

---
+++ databinding/src/main/java/wxgaly/android/databinding/MainActivity.kt
@@ -0,0 +1,19 @@
+package wxgaly.android.databinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import wxgaly.android.databinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
+            this, R.layout.activity_main)
+
+        binding.user = User("Test", "User")
+    }
+}
---
+++ databinding/src/main/java/wxgaly/android/databinding/User.kt
@@ -0,0 +1,9 @@
+package wxgaly.android.databinding
+
+/**
+ *  wxgaly.android.databinding.
+ *
+ * @author Created by WXG on 2020/7/15 11:47 PM.
+ * @version V1.0
+ */
+data class User(val firstName: String, val lastName: String)
